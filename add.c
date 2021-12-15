d1e37126355153478222133d853a469afda7bd2d
omii6696 <96181096+omii6696@users.noreply.github.com>
2021-12-15 17:45:49 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int a,b,sum;
+  printf("Enter 1 number:");
+  scanf("%d",&a);
+  printf("Enter 2 number");
+  scanf("%d,&b");
+  sum=a+b;
+  printf("Sum is:%d,sum");
+}
